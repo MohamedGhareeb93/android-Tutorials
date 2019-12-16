5cd356d1b728c3b00f46be6be3775ddac0cbff71
Sharppy <sharppy0@gmail.com>
2019-12-16 05:49:24 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //undo = new Stack<Integer>();
         undoInt = new Stack<Integer>();
         undoKey = new Stack<Integer>();
 
